d35c5b40564b9f2b03f715eced5c48f1614a8c83
JaredStephens <jbstephens00@gmail.com>
2021-05-19 14:59:39 -0400

    color added to span

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // RANDOM COLOR
 const colors = ["red", "blue", "orange", "yellow", "green", "purple"];
 const btn = document.querySelector("button");
-const color = document.querySelector("color");
+const color = document.querySelector(".color");
 
 // CHANGE BACKGROUND COLOR
 btn.addEventListener("click", function () {
 	const randomNumber = getRandomNumber();
 	console.log(randomNumber);
+
 	document.body.style.backgroundColor = colors[randomNumber];
+	color.textContent = colors[randomNumber];
 });
 function getRandomNumber() {
 	return Math.floor(Math.random() * colors.length);
