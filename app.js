5f8e04ff499d2f857c50b3fb20f10146dd679503
JaredStephens <jbstephens00@gmail.com>
2021-05-19 13:22:03 -0400

    javascript background color

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello");
+// RANDOM COLOR
+const colors = ["red", "blue", "orange", "yellow", "green", "purple"];
+const random = colors.length;
+console.log(random);
+
+// CHANGE BACKGROUND COLOR
+const btn = document
+	.querySelector("button")
+	.addEventListener("click", function () {
+		document.body.style.backgroundColor = "red";
+	});
